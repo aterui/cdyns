10e4b8bf2a78e49bdfc3f92830cf97561a40acce
aterui <hanabi0111@gmail.com>
2024-11-12 13:44:20 -0500

    edit: fn_model()
    `alpha_scale` is now logical input

--- R/utils.R
+++ R/utils.R
@@ -6,29 +6,37 @@
 fn_model <- function(model) {
 
   if (model == "bh") {
-    fun_dyn <- function(r, n1, n2, k, m_int, eps,
-                        stochastic = FALSE, alpha_scale = "scaled") {
+    fun_dyn <- function(r,
+                        n1,
+                        n2,
+                        k,
+                        m_int,
+                        eps,
+                        stochastic = FALSE,
+                        alpha_scale = TRUE) {
 
-      if (alpha_scale == "scaled") {
+      if (alpha_scale) {
 
         n_bar <- (n1 * exp(r)) / (1 + ((exp(r) - 1) / k) * (m_int %*% n2))
 
       } else {
 
-        if (alpha_scale == "unscaled") {
+        n_bar <- (n1 * exp(r)) / (1 + m_int %*% n2)
 
-          n_bar <- (n1 * exp(r)) / (1 + m_int %*% n2)
-
-        } else {
-
-          stop('"alpha_scaled" must be either "scaled" or "unscaled"')
-
-        }
       }
 
       n_hat <- n_bar * exp(eps)
 
-      if (stochastic) y <- rpois(n = length(n_hat), lambda = n_hat) else y <- drop(n_hat)
+      if (stochastic) {
+
+        y <- rpois(n = length(n_hat),
+                   lambda = n_hat)
+
+      } else {
+
+        y <- drop(n_hat)
+
+      }
 
       return(y)
     }
@@ -36,30 +44,38 @@ fn_model <- function(model) {
 
   # ricker
   if (model == "ricker") {
-    fun_dyn <- function(r, n1, n2, k, m_int, eps,
-                        stochastic = FALSE,
-                        alpha_scale = "scaled") {
 
-      if (alpha_scale == "scaled") {
+    fun_dyn <- function(r,
+                        n1,
+                        n2,
+                        k,
+                        m_int,
+                        eps,
+                        stochastic = FALSE,
+                        alpha_scale = TRUE) {
+
+      if (alpha_scale) {
 
         n_bar <- n1 * exp(r * (1 - ((m_int %*% n2) / k)))
 
       } else {
 
-        if (alpha_scale == "unscaled") {
+        n_bar <- n1 * exp(r - m_int %*% n2)
 
-          n_bar <- n1 * exp(r - m_int %*% n2)
-
-        } else {
-
-          stop('"alpha_scaled" must be either "scaled" or "unscaled"')
-
-        }
       }
 
       n_hat <- n_bar * exp(eps)
 
-      if (stochastic) y <- rpois(n = length(n_hat), lambda = n_hat) else y <- drop(n_hat)
+      if (stochastic) {
+
+        y <- rpois(n = length(n_hat),
+                   lambda = n_hat)
+
+      } else {
+
+        y <- drop(n_hat)
+
+      }
 
       return(y)
     }
